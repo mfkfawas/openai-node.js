61b254cb4847c95c4e6f6b8e2ced34a62dd2c5e4
mfkfawas <mfkfawas@gmail.com>
2023-03-19 11:12:37 +0530

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+import readline from "readline";
 import { config } from "dotenv";
-config()
+config();
 
-console.log(process.env);
+import { Configuration, OpenAIApi } from "openai";
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPEN_AI_API_KEY
+  })
+);
+
+const userInterface = new readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+userInterface.prompt();
+userInterface.on("line", async input => {
+  const res = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }]
+  });
+  console.log(res.data.choices[0].message.content);
+  userInterface.prompt();
+});
